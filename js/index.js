e7d17a21cf25b38956d3613103585618590453c6
Bastian B. <bbernst@uni-koblenz.de>
2015-03-09 14:03:03 +0100

    Changed Map tiles
    For better visual experience

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@ function checkMap(){
         map = L.map('map');
 
 
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ',
         id: 'examples.map-i875mjb7'
