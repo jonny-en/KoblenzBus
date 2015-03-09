3fcf8b12a1c056ef5265719a6f2093ff3ec64176
mareif <marcel.reif@web.de>
2015-03-09 14:40:09 +0100

    if routes equally fast -> take shortest

--- js/busMapping.js
+++ js/busMapping.js
@@ -837,10 +837,12 @@ function processData(){
       console.log ("Couldnt load routes");
     } else{
       var minimum;
+      var minimumLength;
       var index=0;
       for (var i=0; i<value.length; i++){
         if (i===0){
           minimum=value[i].route[value[i].route.length-1].times[1];
+          minimumLength=value[i].route.length;
         } else {
           // 23:00 -> 0 -> 1 Uhr Problem
           //Ansonsten funktioniert Direktvergleich von String
@@ -848,7 +850,11 @@ function processData(){
             index=i;
             minimum=value[i].route[value[i].route.length-1].times[1];
           }
-
+          if (minimum===value[i].route[value[i].route.length-1].times[1] && minimumLength>value[i].route.length){
+            index=i;
+            minimumLength=value[i].route.length;
+            minimum=value[i].route[value[i].route.length-1].times[1];
+          }
         }
         
       }
